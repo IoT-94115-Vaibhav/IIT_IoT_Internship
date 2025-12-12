1ca702a222f1958d0b8399bd7633864c66190ea5
vaibhavlande23 <vaibhavsudhakarlande@gmail.com>
2025-12-12 21:27:14 +0530

    que2

---
+++ assigments/day1/question2.py
@@ -0,0 +1,8 @@
+# Addition of Two Numbers
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum = num1 + num2
+
+print("The sum of the two numbers is:", sum)
