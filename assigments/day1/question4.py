8dbbffee59ee6a8476ff42b1f452c9d420e4ed92
vaibhavlande23 <vaibhavsudhakarlande@gmail.com>
2025-12-12 21:35:04 +0530

    que4

---
+++ assigments/day1/question4.py
@@ -0,0 +1,15 @@
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Performing operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2   # assumes num2 is not zero
+
+# Display results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
